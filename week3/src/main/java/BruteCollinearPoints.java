7ab18beaceffb697f21e53d10353f2ee8592ed17
fascynacja <agata.pysz@gmail.com>
2017-02-16 18:45:25 +0100

    week3 assignemnt

---
+++ week3/src/main/java/BruteCollinearPoints.java
@@ -0,0 +1,18 @@
+public class BruteCollinearPoints {
+    public BruteCollinearPoints(Point[] points) {
+    }
+
+    ;
+
+    public int numberOfSegments() {
+        return 4;
+    }
+
+    ;
+
+    public LineSegment[] segments() {
+        return null;
+    }
+
+    ;
+}
